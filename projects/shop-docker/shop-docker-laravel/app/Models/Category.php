2d7855e8de4e5dd503ca2d08f0fa3e07db8f65b9
Derane <vsierhieiev-its24@lll.kpi.ua>
2024-04-07 23:30:39 +0300

    add models

---
+++ projects/shop-docker/shop-docker-laravel/app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(function (Category $category) {
+            $category->slug = $category->slug ?? str($category->title)->slug();
+        });
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
